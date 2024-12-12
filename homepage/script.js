9c9f2c195caca248e8730519ebf767fb6e079fd3
Thiago <tgomes_2@hotmail.com>
2024-12-12 14:15:40 -0300

    feat: impl. forms validation of HomePage

---
+++ homepage/script.js
@@ -0,0 +1,50 @@
+let subscribeBtn = document.getElementById("subscribe-btn");
+
+subscribeBtn.addEventListener('click', function (event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById("email").value.trim();
+    const name = document.getElementById("name").value.trim();
+
+    const emailError = document.getElementById("email-error");
+    const nameError = document.getElementById("name-error");
+
+    emailError.textContent = "";
+    nameError.textContent = "";
+
+    let isValid = true;
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const nameRegex = /^[a-zA-Zá-üÁ-ÜçÇ]{2,20}$/;
+
+    // Validação do email
+    if (!emailRegex.test(email)) {
+        emailError.textContent = "Please enter a valid email address.";
+        isValid = false;
+    }
+
+    
+    if (!nameRegex.test(name)) {
+        nameError.textContent = "Please enter a valid name with at least two characters.";
+        isValid = false;
+    }
+
+    
+    if (isValid) {   
+
+    localStorage.setItem("subscription", JSON.stringify({ email, name }));
+    alert("Subscription successful!");
+
+    document.getElementById("name").value = "";
+    }
+});
+
+
+
+
+
+
+
+
+
+
